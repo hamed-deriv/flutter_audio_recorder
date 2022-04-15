d63960b295813f8a012e7b45def77d8580c6f73f
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-04-15 10:48:47 +0800

    add analysis options

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,9 @@ class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Audio Recorder',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomePage(title: 'Flutter Audio Recorder'),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Flutter Audio Recorder',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: const HomePage(title: 'Flutter Audio Recorder'),
+      );
 }
