18d525ea96dc66ad87b3cc6c8ea473d3a1c7fd6c
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-04-15 10:47:08 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_audio_recorder/home_page.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Audio Recorder',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(title: 'Flutter Audio Recorder'),
+    );
+  }
+}
